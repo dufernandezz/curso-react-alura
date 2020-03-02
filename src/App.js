1fe19effa77c1446b97306d99c7e6a53a0a65865
Eduardo <eduardo.fernandes@matchboxbrasil.com>
2020-03-02 15:44:14 -0300

    Subindo primeira versão do projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Tabela from "./components/Tabela";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    autores: [
+      {
+        nome: "Paulo",
+        livro: "React",
+        preco: "1000"
+      },
+      {
+        nome: "Daniel",
+        livro: "Java",
+        preco: "99"
+      },
+      {
+        nome: "Marcos",
+        livro: "Design",
+        preco: "150"
+      },
+      {
+        nome: "Bruno",
+        livro: "DevOps",
+        preco: "100"
+      }
+    ]
+  };
+  removeLine = idPosition => {
+    const { autores } = this.state;
+    autores.splice(idPosition, 1);
+    this.setState({ autores });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Tabela autores={this.state.autores} removeLine={this.removeLine} />
+      </div>
+    );
+  }
 }
 
 export default App;
